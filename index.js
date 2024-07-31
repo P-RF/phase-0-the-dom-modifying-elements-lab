e1db32739d168c67a7d3d3744cb4b4717a39ca36
Priscila Rodriguez Fresco <priscila.rodriguezfresco@gmail.com>
2024-07-30 20:50:04 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Find element by ID
+const mainElement = document.getElementById('main');
+// Check if 'main' element exists
+if(mainElement) {
+    // Remove 'main' element from DOM
+    mainElement.remove();
+}
+// Create new h1 element
+const newHeader = document.createElement('h1');
+// Set text id to 'vicory' for the <h1> element
+newHeader.id = 'victory'
+// Append the <h1> element to the body
+document.body.appendChild(newHeader);
+//  The 'newHeader' variable that points to the <h1> node has the text "YOUR-NAME is the champion"
+newHeader.textContent = 'Priscila is the champion'
